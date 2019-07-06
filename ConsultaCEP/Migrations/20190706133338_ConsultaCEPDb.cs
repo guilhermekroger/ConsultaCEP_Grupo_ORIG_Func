62a55156e50192421342fe177d07961d5277cc27
Lucas Leonardo <leus12pa@hotmail.com>
2019-07-06 15:30:56 -0300

    Add project files.

---
+++ ConsultaCEP/Migrations/20190706133338_ConsultaCEPDb.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ConsultaCEP.Migrations
+{
+    public partial class ConsultaCEPDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CEP",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Cep = table.Column<string>(nullable: true),
+                    Logradouro = table.Column<string>(nullable: true),
+                    Bairro = table.Column<string>(nullable: true),
+                    Localidade = table.Column<string>(nullable: true),
+                    Uf = table.Column<string>(nullable: true),
+                    ibge = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CEP", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CEP");
+        }
+    }
+}
---
+++ ConsultaCEP/Models/CEP.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsultaCEP.Models
+{
+    public class CEP
+    {
+        public int Id { get; set; }
+        public string Cep { get; set; }
+        public string Logradouro { get; set; }
+        public string Bairro { get; set; }
+        public string Localidade { get; set; }
+        public string Uf { get; set; }
+        public string Ibge { get; set; }
+    }
+}
